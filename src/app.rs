f867d2541d0f540be85e3051fd83672e8f3a9cc4
Lexo Liu <me@lexo.cool>
2023-12-06 16:39:30 +0800

    Make the usage of middleware more convenient

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,15 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, sync::Arc};
 
 use crate::{
     middleware::{Next, SharedMiddleware},
-    Endpoint, Request, Response,
+    Endpoint, Middleware, Request, Response,
 };
 
 /// An App containing endpoint and middlewares.
 #[derive(Default)]
 pub struct App<E: Endpoint> {
     endpoint: E,
-    middlewares: Box<[SharedMiddleware]>,
+    middlewares: Vec<SharedMiddleware>,
 }
 
 impl<E: Endpoint> Debug for App<E> {
@@ -22,12 +22,23 @@ impl<E: Endpoint> Debug for App<E> {
 }
 
 impl<E: Endpoint> App<E> {
-    /// Create an app from endpoint and middlewares.
-    pub const fn new(endpoint: E, middlewares: Box<[SharedMiddleware]>) -> Self {
+    /// Create an app from endpoint.
+    pub const fn new(endpoint: E) -> Self {
         Self {
             endpoint,
-            middlewares,
+            middlewares: Vec::new(),
         }
+    }
+
+    /// Add a shared middleware to this app.
+    pub fn add_middleware(&mut self, middleware: SharedMiddleware) {
+        self.middlewares.push(middleware);
+    }
+
+    /// Add a middleware to this app.
+    pub fn middleware(mut self, middleware: impl Middleware + 'static) -> Self {
+        self.add_middleware(Arc::new(middleware));
+        self
     }
 
     /// Run the app with a provided request.
